65a9678cfe683caafe55127d590fde5355987ccf
alanbrunoscience <alanbrunomath@gmail.com>
2025-03-21 16:14:59 -0300

    :sparkles: Completion of the 'Strings 1' section

---
+++ 02_Strings 1/25_GetMultilineString.js
@@ -0,0 +1,10 @@
+function getMultilineString() {
+
+  return `I am learning JavaScript 
+and I found it to be 
+quite fun!`
+
+}
+
+// Sample usage - do not modify
+console.log(getMultilineString());
---
+++ 02_Strings 1/28_NameVariationsProject.js
@@ -0,0 +1,49 @@
+const readlineSync = require("readline-sync");
+
+function getNumberOfChars(name) {
+  
+  return name.length;
+
+}
+
+function getFirstChar(name) {
+
+  return name[0];
+
+}
+
+function getLastChar(name) {
+  
+  return name.at(-1);
+
+}
+
+function getLower(name) {
+  
+  return name.toLowerCase();
+
+}
+
+function getUpper(name) {
+  
+  return name.toUpperCase();
+
+}
+
+function getCapitalized(name) {
+  
+  return name[0].toUpperCase() + name.substring(1).toLowerCase();
+
+}
+
+const name = readlineSync.question("Enter your name: ");
+
+console.log(`Number of characters: ${getNumberOfChars(name)};`);
+console.log(`First character: ${getFirstChar(name)};`);
+console.log(`Last character: ${getLastChar(name)};`);
+console.log(`Lower case: ${getLower(name)};`);
+console.log(`Upper case: ${getUpper(name)};`);
+console.log(`Capitalize: ${getCapitalized(name)}.`);
+
+// Exporta funções para uso em outros módulos (se necessário)
+module.exports = { getNumberOfChars, getFirstChar, getLastChar, getLower, getUpper, getCapitalized };
