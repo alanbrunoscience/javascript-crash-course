1451180ad08668e497689fa8e1b24fa7382414c4
alanbrunoscience <alanbrunomath@gmail.com>
2025-10-22 21:25:22 -0300

    :sparkles: Add more code examples

---
+++ Sections/02_Strings 1/01_Strings.js
@@ -0,0 +1,15 @@
+/**
+ * Strings
+ * 
+ * → You can create a string in JavaScript by using the double quotes (") or single
+ * quotes (').
+ * 
+ * → Strings are zero-indexed, meaning the first character is at position 0, the second 
+ * at position 1, and so on.
+ * 
+ */
+
+'use strict';
+
+console.log("This is a string!");
+console.log('This is another string!');
